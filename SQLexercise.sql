26cd820b73346587df790bcc853a87f76c39e885
0xVanes <167046429+0xVanes@users.noreply.github.com>
2026-02-15 16:42:24 +0700

    Update SQLexercise.sql

--- SQLexercise.sql
+++ SQLexercise.sql
@@ -50,11 +50,62 @@ GROUP BY Rating
 ORDER BY Rating;
 
 /* NOMOR 6*/
+SHOW DATABASES;
+USE sakila;
+SHOW tables;
+SELECT film.title AS Judul, film.length AS Durasi, language.name AS Bahasa_Film
+FROM film
+JOIN language
+ON film.language_id = language.language_id
+WHERE Judul LIKE '%K'
+LIMIT 15;
 
 /* NOMOR 7*/
+SHOW DATABASES;
+USE sakila;
+SHOW tables;
+DESC actor;
+SELECT film.title, actor.first_name, actor.last_name
+FROM film_actor
+JOIN actor
+ON actor.actor_id = film_actor.actor_id
+JOIN film
+ON film.film_id = film_actor.film_id
+WHERE actor.actor_id = 14;
 
 /* NOMOR 8*/
+SHOW DATABASES;
+USE sakila;
+SHOW tables;
+DESC city;
+SELECT city, country_id
+FROM city
+WHERE city LIKE '%d%a'
+LIMIT 15;
 
 /* NOMOR 9*/
+SHOW DATABASES;
+USE sakila;
+SHOW tables;
+SELECT category.name AS Genre, count(category.name) AS Banyak_Film
+FROM film_category
+JOIN film
+ON film_category.film_id = film.film_id
+JOIN category
+ON category.category_id = film_category.category_id
+GROUP BY Genre
+ORDER BY Banyak_Film;
 
 /* NOMOR 10*/
+SHOW DATABASES;
+USE sakila;
+SHOW tables;
+DESC film;
+SELECT title, description, length, rating
+FROM film
+WHERE title LIKE '%h' AND length > (
+	SELECT AVG(length)
+    FROM film
+)
+ORDER BY title
+LIMIT 10;
