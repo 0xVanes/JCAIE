a9adf1c6563f67933de10f717717359316eb9b58
0xVanes <167046429+0xVanes@users.noreply.github.com>
2026-02-15 15:14:45 +0700

    Create SQLexercise.sql

---
+++ SQLexercise.sql
@@ -0,0 +1,32 @@
+/* NOMOR 1 */
+SHOW DATABASES;
+USE sakila;
+SHOW tables;
+SELECT *
+FROM payment
+LIMIT 10;
+
+/* NOMOR 2*/
+SHOW DATABASES;
+USE sakila;
+SHOW tables;
+SELECT title, release_year, rental_duration
+FROM film
+WHERE title LIKE 'S%'
+LIMIT 10;
+
+/* NOMOR 3*/
+
+/* NOMOR 4*/
+
+/* NOMOR 5*/
+
+/* NOMOR 6*/
+
+/* NOMOR 7*/
+
+/* NOMOR 8*/
+
+/* NOMOR 9*/
+
+/* NOMOR 10*/
