42ef2e7463e63cfbc405f45feaf18907064e7d8a
0xVanes <167046429+0xVanes@users.noreply.github.com>
2026-02-15 16:02:40 +0700

    Update SQLexercise.sql

--- SQLexercise.sql
+++ SQLexercise.sql
@@ -16,10 +16,38 @@ WHERE title LIKE 'S%'
 LIMIT 10;
 
 /* NOMOR 3*/
+SHOW DATABASES;
+USE sakila;
+SHOW tables;
+DESC film;
+SELECT rental_duration AS Durasi_Rental, COUNT(film_id) AS Banyak_Film, ROUND(AVG(length)) AS Rata_Rata_Durasi_Film
+FROM film
+GROUP BY Durasi_Rental
+ORDER BY Durasi_Rental;
 
 /* NOMOR 4*/
+SHOW DATABASES;
+USE sakila;
+SHOW tables;
+DESC film;
+SELECT title, length, rating
+FROM film
+WHERE length > (
+	SELECT AVG(length)
+    FROM film
+)
+ORDER BY length DESC
+LIMIT 25;
 
 /* NOMOR 5*/
+SHOW DATABASES;
+USE sakila;
+SHOW tables;
+DESC film;
+SELECT rating AS Rating, MAX(replacement_cost) AS Replacement_Cost_Tertinggi, MIN(rental_rate) AS Rental_Rate_Terendah, AVG(length) as Rata_Rata_Durasi
+FROM film
+GROUP BY Rating
+ORDER BY Rating;
 
 /* NOMOR 6*/
 
